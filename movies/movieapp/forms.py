400f97ae76a891fe2deb97b6222e51eb745fd7c0
wkdfury <aadilfury@gmail.com>
2021-08-07 15:59:52 +0530

    addit

---
+++ movies/movieapp/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Movie
+
+class Movieform(forms.ModelForm):
+    class Meta:
+        model=Movie
+        fields=['name',
+                'desc',
+                'year',
+                'img']
+
